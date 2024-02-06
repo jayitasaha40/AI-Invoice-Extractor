cb4be13adf03e349fef9998de43f0fca5f6008de
jayitasaha40 <jayitasaha40@gmail.com>
2024-02-06 22:05:22 +0530

    Config driven AWS

--- AWS.py
+++ AWS.py
@@ -16,7 +16,7 @@ def extract(input_file):
  
     text = ""
  
-    client = boto3.client('textract',region_name='ca-central-1', aws_access_key_id='ASIA37NKDWJ23JMHUKOU', aws_secret_access_key='055XVdaCVvdRy19FTZAfyqwuyfWWgxAi0nao5dTJ', aws_session_token='IQoJb3JpZ2luX2VjEHYaDGNhLWNlbnRyYWwtMSJHMEUCIQDyNvpPNIbZn/AHFHa4WEJ9z+hVYeuRY01TVTYiPu3G2AIgCzf8vdtPI4b2+S8e6fQ1ba8+xBIF6x/Jha4o1gwa30cqiwMIQBADGgw4MjMzNzk4NjYyMjkiDPMSHRlz+L/boL3+LCroAqy0JgrKWA14LW9kYcd3QULGFObiH1cdFdbyQ+xuPn8TRNbpyrNnjaC3EwxiMZqa2qLejpkSoKarlHlzIz7HsIAWYB7zV8fNz8/+DX3YN1z78+0qG8DdP6sFBsNZwJKq/vkCZ3ltmgU5ClklNwa2ugSofPNQFRyYnR3gdmCzKyeQhYzpqxebsGimofqOcvVUEBDj5e3hftA0jC/HpP1JK+Ml44pWhByIulj0x2a3p2I68PKvPQ/alEjIn+2iTJ1hgRWA9i+dkyF98rOJg84IsjvS4UNGYOfFIXRRomKdOceZj3avrbQlrBijOHPySIa9Tc33bZ9U57hnzGwTrBsxm7tI45uV1/dGd54gsj4u5zdWYTkBxcWz8clbgeHmykmWEXWRA+lNZEf0720o7u3Aa6/2f0xZLcpNTADSJL65sZrpog/CUppfNbUHr154EfkpYR656MvonHE8+SMF2DEsB4VqJavSBKOCOzCz9OytBjqmAcIrPgja/fBfJ036kQ+Fjxz0ArRei18tntgguBz/on32h0eGbQX9WhkIvo/qGuWAGRj76JEHQEQjjPholKoK6rmuI4dYdWLbOyf5NUZhK9ja/lBaErwtJbalnrEoK2nzdLJijesCcOISZHiSEmcsHg4hJtSs/UbUGhK7cLUTok+G7tzV9gFqPRchtza+Q+7R1bTtCe6pGmrqR5geaBbrao8OsHd6cyM=')
+    client = boto3.client('textract',region_name='ca-central-1', aws_access_key_id=config["AWS"]["aws_access_key_id"], aws_secret_access_key= config["AWS"]["aws_secret_access_key"], aws_session_token= config["AWS"]["aws_session_token"]) 
  
     if is_pdf_file(input_file):
  
